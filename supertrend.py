b3164983949ff0e62a96e1675eed191731b9241c
Cielight0 <84953788+Cielight0@users.noreply.github.com>
2021-05-31 22:32:37 +0200

    Update supertrend.py

--- supertrend.py
+++ supertrend.py
@@ -2,19 +2,23 @@ import ccxt
 import config
 import schedule
 import pandas as pd
-pd.set_option('display.max_rows', None)
+import talib
+
+pd.set_option('display.max_rows', None, "display.max_columns", None)
 
 import warnings
+
 warnings.filterwarnings('ignore')
 
 import numpy as np
 from datetime import datetime
 import time
 
-exchange = ccxt.binanceus({
+exchange = ccxt.binance({
     "apiKey": config.BINANCE_API_KEY,
     "secret": config.BINANCE_SECRET_KEY
 })
+
 
 def tr(data):
     data['previous_close'] = data['close'].shift(1)
@@ -26,11 +30,34 @@ def tr(data):
 
     return tr
 
+def rsi(df, RSI_PERIOD =14):
+    np_closes = df['close']
+    rsi = talib.RSI(np_closes, RSI_PERIOD)
+    df['rsi']=rsi
+    return rsi
+
+def adx(df, ADX_PERIOD=14):
+    high = df['high']
+    low = df['low']
+    close = df['close']
+    adx = talib.ADX(high, low, close, ADX_PERIOD)
+    df['adx'] = adx
+    return adx
+
+def psar(df, acceleration=0.02,maximum=0.2):
+    high = df['high']
+    low = df['low']
+    psar = talib.SAR(high, low, acceleration, maximum)
+    df['psar'] = psar
+    return psar
+
+
 def atr(data, period):
     data['tr'] = tr(data)
     atr = data['tr'].rolling(period).mean()
 
     return atr
+
 
 def supertrend(df, period=7, atr_multiplier=3):
     hl2 = (df['high'] + df['low']) / 2
@@ -54,15 +81,19 @@ def supertrend(df, period=7, atr_multiplier=3):
 
             if not df['in_uptrend'][current] and df['upperband'][current] > df['upperband'][previous]:
                 df['upperband'][current] = df['upperband'][previous]
-        
+
+    #print(df)
     return df
 
 
 in_position = False
 
+
 def check_buy_sell_signals(df):
     global in_position
-
+    rsi(df,14)
+    adx(df,14)
+    psar(df)
     print("checking for buy and sell signals")
     print(df.tail(5))
     last_row_index = len(df.index) - 1
@@ -71,35 +102,34 @@ def check_buy_sell_signals(df):
     if not df['in_uptrend'][previous_row_index] and df['in_uptrend'][last_row_index]:
         print("changed to uptrend, buy")
         if not in_position:
-            order = exchange.create_market_buy_order('ETH/USD', 0.05)
+            order = exchange.create_market_buy_order('ETH/BUSD', 0.05)
             print(order)
             in_position = True
         else:
             print("already in position, nothing to do")
-    
+
     if df['in_uptrend'][previous_row_index] and not df['in_uptrend'][last_row_index]:
         if in_position:
             print("changed to downtrend, sell")
-            order = exchange.create_market_sell_order('ETH/USD', 0.05)
+            order = exchange.create_market_sell_order('ETH/BUSD', 0.05)
             print(order)
             in_position = False
         else:
             print("You aren't in position, nothing to sell")
 
+
 def run_bot():
     print(f"Fetching new bars for {datetime.now().isoformat()}")
-    bars = exchange.fetch_ohlcv('ETH/USDT', timeframe='1m', limit=100)
+    bars = exchange.fetch_ohlcv('ETH/BUSD', timeframe='1m', limit=100)
     df = pd.DataFrame(bars[:-1], columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
     df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
-
     supertrend_data = supertrend(df)
-    
-    check_buy_sell_signals(supertrend_data)
 
+    check_buy_sell_signals(supertrend_data)
 
 schedule.every(10).seconds.do(run_bot)
 
-
 while True:
+
     schedule.run_pending()
     time.sleep(1)
